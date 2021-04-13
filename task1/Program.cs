164dc27009cc3379f5b5ad0fcaf79dedbd5c5e1e
doomkinn-geek <doomkinn@yandex.ru>
2021-04-13 19:52:06 +0300

    ...

--- task1/Program.cs
+++ task1/Program.cs
@@ -16,18 +16,42 @@ namespace task1
             int pageCounter = 0;
             foreach (Process p in Process.GetProcesses())
             {
-                string memSize = ToPrettySize(p.);
+                string memSize = ToPrettySize(p.PrivateMemorySize);
                 Console.WriteLine("{0, -38} {1, -6} {2, -18} {3, -10} {4, -12}",p.ProcessName, p.Id, p.MachineName, p.SessionId, memSize);
                 pageCounter++;
                 if(pageCounter > 25)
                 {
                     Console.WriteLine("Press Enter to print other page");
                     Console.Read();                    
-                    //Console.WriteLine("Process name                              PID Session Name         Session#    Mem Usage");
-                    //Console.WriteLine("====================================== ====== ================== ========== ============");
                     pageCounter = 0;
                 }
             }
+
+            Console.Write("Enter process to stop (ID or name)");
+            string processName = Console.ReadLine();
+
+            
+            int processId = 0;
+
+            try
+            {
+                processId = Convert.ToInt32(processName);
+                Process p = Process.GetProcessById(processId);
+                p.Kill();
+            }
+            catch(Exception)
+            {
+                processId = 0;
+            }
+
+            if (processId == 0)
+            {
+                foreach (Process p in Process.GetProcessesByName(processName))
+                {
+                    p.Kill();
+                }
+            }
+
         }
         public static string ToPrettySize(this long value, int decimalPlaces = 0)
         {
